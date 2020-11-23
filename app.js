a6fc30be0eb922d6afdc9d4db0c934b3a7b8079c
LetTheRiverRun <hoanggiang1123@gmail.com>
2020-11-23 13:26:17 +0800

    fixed

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 const a = 'testOne'
-<<<<<<< HEAD
 const c = '12414'
 const d = 235252
-=======
 const b = '21414'
-<<<<<<< HEAD
->>>>>>> 8a31a8deca4968f02016942f6d21a36a7ce21ece
-=======
 const e = 'add e'
->>>>>>> 64b1c83fe83e7ee7ee682da1b8293c057bd6fcaa
